10ed644b79190fc42b580be01942ab32f5fa2021
HenryAlexander2 <henry.es198@gmail.com>
2025-12-05 20:32:42 -0500

    Estructura: limpiar App + main y agregar carpetas components/pages/data/styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // vacío por ahora
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
